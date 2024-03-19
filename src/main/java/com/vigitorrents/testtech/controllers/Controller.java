c9d0555e01f2d49a269f75e7d43b99402f08f18f
Pierre LM <pierrelm@hotmail.com>
2024-03-19 11:32:30 +0100

    Improve controller

--- src/main/java/com/vigitorrents/testtech/controllers/Controller.java
+++ src/main/java/com/vigitorrents/testtech/controllers/Controller.java
@@ -8,6 +8,7 @@ import com.vigitorrents.testtech.repositories.SubscriptionsRepository;
 import com.vigitorrents.testtech.services.SmsSender;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import pl.smsapi.exception.SmsapiException;
 
@@ -18,28 +19,25 @@ import java.util.stream.StreamSupport;
 @RestController
 @Tag(name = "Vigi-Torrents")
 class Controller {
-    private final SignalementsRepository signalementsRepository;
-    private final SubscriptionsRepository subscriptionsRepository;
-    private final SmsSender smsSender;
+    @Autowired
+    private SignalementsRepository signalementsRepository;
+    @Autowired
+    private SubscriptionsRepository subscriptionsRepository;
+    @Autowired
+    private SmsSender smsSender;
 
     private int indexPosition = 0;
-    private List<Position> positions = List.of(
+    private final List<Position> positions = List.of(
             new Position(45.0, 5.0),
             new Position(45.1, 5.1),
             new Position(45.2, 5.2)
     );
 
-    public Controller(SignalementsRepository signalementsRepository,
-                      SubscriptionsRepository subscriptionsRepository,
-                      SmsSender smsSender) {
-        this.signalementsRepository = signalementsRepository;
-        this.subscriptionsRepository = subscriptionsRepository;
-        this.smsSender = smsSender;
-    }
+    public Controller() {}
 
     @GetMapping(value = {"/api/position", "/_ah/warmup"})
     @CrossOrigin
-    @Operation(summary = "La position est piochée aléatoirement le long du torrent à l'amont", description = "La position est piochée aléatoirement le long du torrent à l'amont")
+    @Operation(summary = "La position est piochée aléatoirement le long du torrent à l'amont")
     Position getPosition() {
         indexPosition++;
         return positions.get(indexPosition % positions.size());
@@ -47,37 +45,42 @@ class Controller {
 
     @GetMapping(value = {"/api/informations"})
     @CrossOrigin
-    @Operation(summary = "Retourne des infos diverses en texte libre", description = "Retourne des infos diverses en texte libre")
+    @Operation(summary = "Retourne des infos diverses en texte libre")
     String getInformations() {
         return "Blabla";
     }
 
     @PostMapping(value = {"/api/signalement"})
     @CrossOrigin
+    @Operation(summary = "Enregistre un nouveau signalement")
     Signalement postSignalement(@RequestBody Signalement signalement) {
         return signalementsRepository.save(signalement);
     }
 
     @GetMapping(value = {"/api/signalements"})
     @CrossOrigin
+    @Operation(summary = "Récupère tous les signalements enregistrés")
     Iterable<Signalement> getSignalements() {
         return signalementsRepository.findAll();
     }
 
     @PostMapping(value = {"/api/subscription"})
     @CrossOrigin
+    @Operation(summary = "Enregistre une nouvelle souscription")
     Subscription postSubscription(@RequestBody Subscription subscription) {
         return subscriptionsRepository.save(subscription);
     }
 
     @GetMapping(value = {"/api/subscriptions"})
     @CrossOrigin
+    @Operation(summary = "Récupère toutes les souscriptions enregistrées")
     Iterable<Subscription> getSubscriptions() {
         return subscriptionsRepository.findAll();
     }
 
     @DeleteMapping(value = {"/api/subscription"})
     @CrossOrigin
+    @Operation(summary = "Supprime les souscriptions liées à un numéro de téléphone")
     void deleteSubscription(@RequestParam String mobileNumber) {
         Stream<Subscription> subscriptionsToDelete = StreamSupport.stream(subscriptionsRepository.findAll().spliterator(), false)
                 .filter(it -> it.getMobile().equals(mobileNumber));
@@ -87,6 +90,7 @@ class Controller {
 
     @PostMapping(value = {"/api/sendSMS"})
     @CrossOrigin
+    @Operation(summary = "Envoie un SMS")
     String sensSMS(@RequestParam String mobileNumber, @RequestParam String message, @RequestParam String accessToken) throws SmsapiException {
         return smsSender.send(mobileNumber, message, accessToken).getList().get(0).getStatus();
     }
